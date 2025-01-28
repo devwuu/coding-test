a59dcad09b08dc34543d5fbc085a266237b1c3ed
devwuu <dev.dwjung@gmail.com>
2025-01-28 16:42:58 +0900

    섹션 10

--- lecture/section10/35.py
+++ lecture/section10/35.py
@@ -1,18 +1,49 @@
+dx = [0, 0, -1, 1]
+dy = [1, -1, 0, 0]
+
 def other_solution_1(board):
     answer = 0
 
+    for i in range(0, 5):
+        for j in range(0, 5):
+            if board[i][j] == 1:
+                answer += 1
+                other_solution_1_dfs(i, j, board)
+
     return answer
 
+def other_solution_1_dfs(i, j, board):
+    board[i][j] = 0  # 방문 지점 체크 -> 무한 재귀 방지, dfs 함수를 호출하는 시점에서 이미 == 1 조건을 만족한 것이기 때문에 굳이 if 문 안으로 밀어넣을 필요 없음
+    for d in range(0, 4):
+        nx = i + dx[d]
+        ny = j + dy[d]
+        if 0 <= nx < 5 and 0 <= ny < 5 and board[nx][ny] == 1:
+            other_solution_1_dfs(nx, ny, board)
+
+#####
 
 def my_solution_1(board):
     answer = 0
 
+    for i in range(0, 5):
+        for j in range(0, 5):
+            if board[i][j] == 1:
+                answer += 1
+                board[i][j] = 2 # 방문 지점 체크 -> 무한 재귀 방지
+                my_solution_1_adjoin(board, i, j)
+
     return answer
+
+def my_solution_1_adjoin(board, i, j): # 최초의 영역에서 인접한 영역을 모두 2로 만들기 + 인접한 영역의 인접한 영역도 2로 만들기
+    for d in range(0, 4):
+        if 0 <= i + dx[d] < 5 and 0 <= j + dy[d] < 5 and board[i + dx[d]][j + dy[d]] == 1:
+            board[i + dx[d]][j + dy[d]] = 2 # 방문 지점 체크 -> 무한 재귀 방지
+            my_solution_1_adjoin(board, i + dx[d], j + dy[d])
 
 
 if __name__ == '__main__':
-    print(my_solution_1([[0, 1, 1, 0, 0], [0, 1, 1, 0, 0], [0, 1, 0, 0, 0], [0, 0, 0, 1, 0], [0, 0, 1, 1, 0]]))
-    print(my_solution_1([[1, 1, 1, 0, 1], [1, 1, 1, 0, 1], [0, 0, 1, 0, 0], [1, 1, 0, 1, 0], [1, 0, 1, 0, 0]]))
-    print(my_solution_1([[0, 0, 1, 0, 0], [0, 1, 1, 0, 0], [0, 1, 0, 0, 0], [1, 0, 0, 1, 0], [0, 0, 1, 1, 0]]))
-    print(my_solution_1([[0, 0, 0, 0, 1], [0, 0, 1, 0, 0], [0, 1, 0, 1, 0], [0, 0, 0, 1, 0], [0, 0, 1, 0, 0]]))
+    print(other_solution_1([[0, 1, 1, 0, 0], [0, 1, 1, 0, 0], [0, 1, 0, 0, 0], [0, 0, 0, 1, 0], [0, 0, 1, 1, 0]]))
+    print(other_solution_1([[1, 1, 1, 0, 1], [1, 1, 1, 0, 1], [0, 0, 1, 0, 0], [1, 1, 0, 1, 0], [1, 0, 1, 0, 0]]))
+    print(other_solution_1([[0, 0, 1, 0, 0], [0, 1, 1, 0, 0], [0, 1, 0, 0, 0], [1, 0, 0, 1, 0], [0, 0, 1, 1, 0]]))
+    print(other_solution_1([[0, 0, 0, 0, 1], [0, 0, 1, 0, 0], [0, 1, 0, 1, 0], [0, 0, 0, 1, 0], [0, 0, 1, 0, 0]]))
 
--- lecture/section10/36.py
+++ lecture/section10/36.py
@@ -1,20 +1,59 @@
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+cnt = 0
 def other_solution_1(board):
     answer = []
 
+    for i in range(0, 5):
+        for j in range(0, 5):
+            if board[i][j] == 1:
+                global cnt
+                cnt = 0
+                other_solution_1_dfs(i, j, board)
+                answer.append(cnt)
 
     return answer
 
+def other_solution_1_dfs(i, j, board):
+    board[i][j] = 0
+    global cnt
+    cnt += 1 # 호출된 시점 == 1개가 발견되었다는 의미이기 때문에 최초에 +1을 해준다
+    for d in range(0, 4):
+        nx = i + dx[d]
+        ny = j + dy[d]
+        if 0 <= nx < 5 and 0 <= ny < 5 and board[nx][ny] == 1:
+            other_solution_1_dfs(nx, ny, board)
 
+##########
+
+pixel = 1
 def my_solution_1(board):
     answer = []
-
+    for i in range(0, 5):
+        for j in range(0, 5):
+            if board[i][j] == 1:
+                global pixel
+                pixel = 1
+                my_solution_1_adjoin(board, i, j)
+                answer.append(pixel)
 
     return answer
+
+def my_solution_1_adjoin(board, i, j):
+    board[i][j] = 0
+    for d in range(0, 4):
+        nx = i + dx[d]
+        ny = j + dy[d]
+        if 0 <= nx < 5 and 0 <= ny < 5 and board[nx][ny] == 1:
+            global pixel
+            pixel += 1
+            my_solution_1_adjoin(board, nx, ny)
+
 
 
 if __name__ == '__main__':
-    print(my_solution_1([[0, 1, 1, 0, 0], [0, 1, 1, 0, 0], [0, 1, 0, 0, 0], [0, 0, 0, 1, 0], [0, 0, 1, 1, 0]]))
-    print(my_solution_1([[1, 1, 1, 0, 1], [1, 1, 1, 0, 1], [0, 0, 1, 0, 0], [1, 1, 0, 1, 0], [1, 0, 1, 0, 0]]))
-    print(my_solution_1([[0, 0, 1, 0, 0], [0, 1, 1, 0, 0], [0, 1, 0, 0, 0], [1, 0, 0, 1, 0], [0, 0, 1, 1, 0]]))
-    print(my_solution_1([[0, 0, 0, 0, 1], [0, 0, 1, 0, 0], [0, 1, 0, 1, 0], [0, 0, 0, 1, 0], [0, 0, 1, 0, 0]]))
+    print(other_solution_1([[0, 1, 1, 0, 0], [0, 1, 1, 0, 0], [0, 1, 0, 0, 0], [0, 0, 0, 1, 0], [0, 0, 1, 1, 0]]))
+    print(other_solution_1([[1, 1, 1, 0, 1], [1, 1, 1, 0, 1], [0, 0, 1, 0, 0], [1, 1, 0, 1, 0], [1, 0, 1, 0, 0]]))
+    print(other_solution_1([[0, 0, 1, 0, 0], [0, 1, 1, 0, 0], [0, 1, 0, 0, 0], [1, 0, 0, 1, 0], [0, 0, 1, 1, 0]]))
+    print(other_solution_1([[0, 0, 0, 0, 1], [0, 0, 1, 0, 0], [0, 1, 0, 1, 0], [0, 0, 0, 1, 0], [0, 0, 1, 0, 0]]))
 
